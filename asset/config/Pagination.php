03598fb239d1cfa1b65af6b62b001a80045520f7
briandumet <brian.dumet@gmail.com>
2018-05-24 15:26:29 +0700

    oop-pertemuan7

--- asset/config/Pagination.php
+++ asset/config/Pagination.php
@@ -1,51 +1,55 @@
 <?php
 namespace Config;
 class Pagination{
-function Paginate($values,$page,$per_page){
-$total_values = count($values);
 
-if(isset($_GET['page'])){
-$current_page = $_GET['page'];
-}else{
-$current_page = 1;
-}
-$counts = ceil($total_values / $per_page);
-$param1 = ($current_page - 1) * $per_page;
-$this->data = array_slice($values,$param1,$per_page);
+		function Paginate($values,$page,$per_page)
+		{
+		
+		$total_values = count($values);
+		
+			if(isset($_GET['page'])){
+			$current_page = $_GET['page'];
+			}else{
+			$current_page = 1;
+			}
 
-$nextOlder  = "&larr; Older";
-$prevNewer  = "Newer &rarr;";
+		$counts = ceil($total_values / $per_page);
+		$param1 = ($current_page - 1) * $per_page;
+		$this->data = array_slice($values,$param1,$per_page);
 
-$out = "<ul class='pagination justify-content-center mb-4'>";
+		$nextOlder  = "&larr; Older";
+		$prevNewer  = "Newer &rarr;";
 
-// older
-if($param1<($counts-1)) {
-	$out.= "<li class='page-item'><a class='page-link' href=\"" . $page . "" .($current_page+1) . "\">" . $nextOlder . "</a></li>";
-}
-else {
-	$out.= "<li class='page-item disabled'><a class='page-link' href='#'>" . $nextOlder . "</a></li>";
-}
+		$out = "<ul class='pagination justify-content-center mb-4'>";
 
-// newer
-if($param1==0) {
-	$out.= "<li class='page-item disabled'><a class='page-link' href='#'>" . $prevNewer . "</a></li>";
-}
-else {
-	$out.= "<li class='page-item'><a class='page-link' href=\"" . $page . "" . ($current_page-1) . "\">" . $prevNewer . "</a></li>";
-}
+			// older
+			if($param1<($counts-1)) {
+				$out.= "<li class='page-item'><a class='page-link' href=\"" . $page . "" .($current_page+1) . "\">" . $nextOlder . "</a></li>";
+			}
+			else {
+				$out.= "<li class='page-item disabled'><a class='page-link' href='#'>" . $nextOlder . "</a></li>";
+			}
 
-$out.= "</ul>";
-return $out;
+			// newer
+			if($param1==0) {
+				$out.= "<li class='page-item disabled'><a class='page-link' href='#'>" . $prevNewer . "</a></li>";
+			}
+			else {
+				$out.= "<li class='page-item'><a class='page-link' href=\"" . $page . "" . ($current_page-1) . "\">" . $prevNewer . "</a></li>";
+			}
 
-// for($x=1; $x<= $counts; $x++){
-// $numbers[] = $x;
-// }
-// return $numbers;
-}
+		$out.= "</ul>";
+		return $out;
 
-function fetchResult(){
-$resultsValues = $this->data;
-return $resultsValues;
-}
+		// for($x=1; $x<= $counts; $x++){
+		// $numbers[] = $x;
+		// }
+		// return $numbers;
+		}
+
+		function fetchResult(){
+		$resultsValues = $this->data;
+		return $resultsValues;
+		}
 
 }
